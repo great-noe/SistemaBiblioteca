e59898e019de2e64f14604035394ddb77b2047d3
juasimpa-dotcom <juasimpa@gmail.com>
2026-01-14 23:38:05 -0400

    Create CategoriaRepository.cs
    Para listar catalogos

---
+++ SistemaBiblioteca.AccesoDatos/Repositorios/CategoriaRepository.cs
@@ -0,0 +1,7 @@
+public class CategoriaRepository
+{
+    private readonly BibliotecaContext _context;
+    public CategoriaRepository(BibliotecaContext context) => _context = context;
+
+    public List<Categoria> ObtenerActivas() => _context.Categorias.Where(c => c.Activo).ToList();
+}
